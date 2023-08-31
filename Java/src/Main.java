0f8cfc702e5a6284e46b3ddadd6a8cb6dc9a023f
domnru <dominik.business@tutanota.de>
2023-08-31 12:18:22 +0200

    Moved full project code into Main.java

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,6 +1,44 @@
-import algorithms.BubbleSort;
-import algorithms.QuickSort;
-import algorithms.SortContext;
+// Schnittstelle für die Sortierstrategie
+interface SortStrategy {
+    void sort(int[] array);
+}
+
+// Konkrete Strategie: Bubble Sort
+class BubbleSort implements SortStrategy {
+    @Override
+    public void sort(int[] array) {
+        // Implementierung des Bubble Sort Algorithmus
+        // ...
+        System.out.println("Bubble Sort angewendet.");
+    }
+}
+
+// Konkrete Strategie: Quick Sort
+class QuickSort implements SortStrategy {
+    @Override
+    public void sort(int[] array) {
+        // Implementierung des Quick Sort Algorithmus
+        // ...
+        System.out.println("Quick Sort angewendet.");
+    }
+}
+
+// Kontext, der die Sortierstrategie verwendet
+class SortContext {
+    private SortStrategy strategy;
+
+    public SortContext(SortStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void setStrategy(SortStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void executeSort(int[] array) {
+        strategy.sort(array);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
--- Java/src/strategy/SortStrategy.java
+++
@@ -1,6 +0,0 @@
-package strategy;
-
-// Schnittstelle für die Sortierstrategie
-public interface SortStrategy {
-    void sort(int[] array);
-}
